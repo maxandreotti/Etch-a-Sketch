35b94729f8006d7b17e347be8570811e5f11c681
Massimo Andreotti <277351410+maxandreotti@users.noreply.github.com>
2026-06-18 22:40:32 +0200

    COMPITO COMPLETATO, DA FARE LE TASK EXTRA

---
+++ script.js
@@ -0,0 +1,54 @@
+
+const input = document.querySelector("#input");
+const conferma = document.querySelector("#conferma");
+const contenitore = document.querySelector("#contenitore");
+const par = document.querySelector("#par");
+
+conferma.addEventListener("click", () => {
+  if (+input.value > 100 || +input.value < 1) {
+    par.textContent = "Inserisci un numero valido"
+  } else {
+    par.textContent = "";
+    contenitore.innerHTML = "";
+    let quantità = +input.value;
+    let larghezza = 480 / quantità;
+    creaGriglia(larghezza, quantità);
+    input.value = "";
+  }
+})
+
+
+function creaGriglia(larghezza, quantità) {
+  /*ciclo per i div verticali */
+  for (let i = 0; i < quantità; i++) {
+    const div1 = document.createElement("div");
+    contenitore.appendChild(div1);
+    div1.classList.add("div1");
+    /*ciclo per i div orizzonatali */
+    for (let j = 0; j < quantità; j++) {
+      const div2 = document.createElement("div");
+      div1.appendChild(div2);
+      div2.classList.add("div2");
+      div2.style.height = larghezza + "px";
+        div2.style.width = larghezza + "px";
+      div2.addEventListener("mouseover", () => {
+        div2.style.backgroundColor = "black"
+      })
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
