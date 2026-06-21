bdf71ea56bdad1c37022013aa53cc7db4ca5a3ce
Massimo Andreotti <277351410+maxandreotti@users.noreply.github.com>
2026-06-21 10:53:41 +0200

    COMPLETATI I CREDITI EXTRA DELL'ESERCIZIO

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ conferma.addEventListener("click", () => {
 
 
 function creaGriglia(larghezza, quantità) {
+
   /*ciclo per i div verticali */
   for (let i = 0; i < quantità; i++) {
     const div1 = document.createElement("div");
@@ -31,15 +32,24 @@ function creaGriglia(larghezza, quantità) {
       div1.appendChild(div2);
       div2.classList.add("div2");
       div2.style.height = larghezza + "px";
-        div2.style.width = larghezza + "px";
+      div2.style.width = larghezza + "px";
+      let opacità = 0.1;
+      let sfondo = coloreRandom()
       div2.addEventListener("mouseover", () => {
-        div2.style.backgroundColor = "black"
+        div2.style.backgroundColor = sfondo;
+        div2.style.opacity = opacità;
+        if (opacità < 1) opacità += 0.1;
       })
     }
   }
 }
 
-
+function coloreRandom() {
+  let numero1 = Math.floor(Math.random() * 256);
+  let numero2 = Math.floor(Math.random() * 256);
+  let numero3 = Math.floor(Math.random() * 256);
+  return `rgb(${numero1}, ${numero2}, ${numero3})`;
+}
 
 
 
